1682e1a5374fd83a11d3750a2fb3879e800eb214
Séverine Boré <severinebore76@gmail.com>
2026-03-26 19:40:14 +0100

    feat: pages statiques (about, mentions, cgv, contact)

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PagesController extends AbstractController
+{
+    #[Route('/a-propos', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('pages/about.html.twig');
+    }
+
+    #[Route('/mentions-legales', name: 'app_mentions')]
+    public function mentions(): Response
+    {
+        return $this->render('pages/mentions.html.twig');
+    }
+
+    #[Route('/cgv', name: 'app_cgv')]
+    public function cgv(): Response
+    {
+        return $this->render('pages/cgv.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig');
+    }
+}
